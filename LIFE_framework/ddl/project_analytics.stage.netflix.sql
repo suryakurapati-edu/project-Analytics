24798de61a3eaf94b4f1cf228fc6790d745b0fa9
Saranya-Munikannaiah <x23434821@student.ncirl.ie>
2025-04-13 22:05:46 +0530

    modified data_types and config names

---
+++ LIFE_framework/ddl/project_analytics.stage.netflix.sql
@@ -0,0 +1,16 @@
+CREATE TABLE project_analytics.stage.netflix (
+    show_id         VARCHAR(10) NOT NULL,
+    type            VARCHAR(20) NOT NULL,
+    title           VARCHAR(100) NOT NULL,
+    director        VARCHAR(50) NOT NULL,
+    cast            VARCHAR(100) NOT NULL,
+    country         VARCHAR(50) NOT NULL,
+    date_added      DATE TIMESTAMP,
+    release_year    INT
+    duration_min    INT
+    duration_season INT
+    rating          VARCHAR(20) NOT NULL,
+    categories      VARCHAR(50) NOT NULL,
+    description     VARCHAR(255) NOT NULL,
+    load_timestamp  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
--- LIFE_framework/ddl/project_analytics.stage.products.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE project_analytics.stage.products (
-    products_sk     UUID PRIMARY KEY,
-    product_id     Varchar UNIQUE,
-    name      VARCHAR(255),
-    category       VARCHAR(255),
-    price           DECIMAL(10,2),
-    load_timestamp  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
